5cd915b99685c2d29e9e8329e698c78ac1335117
Adrian <adrian2679@poczta.onet.pl>
2025-11-27 11:25:29 +0100

    Added axios config

---
+++ src/services/api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { config } from 'process';
+
+const API_URL = 'http://localhost:8080/api';
+
+const api = axios.create({
+    baseURL: API_URL,
+    headers: {
+        'Content-Type' : 'application/json'
+    }
+});
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('jwt_token');
+        if(token) {
+            config.headers['Authorization'] = 'Bearer ${token}';
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default api;
